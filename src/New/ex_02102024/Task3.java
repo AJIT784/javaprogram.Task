ef2318f6898ae2b4c34617924227c9f53f3b7c44
Ajit Singh <ajitrana598@gmail.com>
2024-10-05 10:36:46 +0530

    My Files

---
+++ src/New/ex_02102024/Task3.java
@@ -0,0 +1,20 @@
+package New.ex_02102024;
+
+public class Task3
+{
+    public static void main (String[]args)
+    {
+        System.out.println("Simple Calculator:");
+        System.out.println("Add two Numbers:");
+        System.out.println(30+15);
+        System.out.println("Subtract two Numbers:");
+        System.out.println(20-15);
+        System.out.println("Multiply two Numbers:");
+        System.out.println(20*15);
+        System.out.println("Divide two Numbers:");
+        System.out.println(10/2);
+
+
+        }
+    }
+
---
+++ src/New/ex_02102024/Task4.java
@@ -0,0 +1,13 @@
+package New.ex_02102024;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+
+    }
+    }
