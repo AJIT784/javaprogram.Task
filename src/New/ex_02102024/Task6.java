d93601a9f0dd03227167d8b284540548cc241613
Ajit Singh <ajitrana598@gmail.com>
2024-10-13 12:59:03 +0530

    New file of java task

---
+++ src/New/ex_02102024/Task6.java
@@ -0,0 +1,17 @@
+package New.ex_02102024;
+
+public class Task6 {
+    public static void main(String[] args) {
+
+        int e = 10;
+        int g = 3;
+        int h = 5;
+        System.out.println("e > g: " + (e > g));
+        System.out.println("e < g: " + (e < g));
+        System.out.println("e >= g: " + (e >= g));
+        System.out.println("e <= g: " + (e <= g));
+        System.out.println("e == h: " + (e == h));
+        System.out.println("e != h: " + (e != h));
+    }
+}
+
