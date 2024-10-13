9dfa1aafc3c7742cb97272734c7a840e52c133b0
Ajit Singh <ajitrana598@gmail.com>
2024-10-13 12:44:35 +0530

    New file of java

---
+++ src/New/ex_02102024/Task5.java
@@ -0,0 +1,11 @@
+package New.ex_02102024;
+
+public class Task5 {
+   public static void main (String[] args) {
+   int a = 10;
+   int b = 20;
+   Boolean c = a > b ;
+   System.out.println(c);
+
+    }
+}
